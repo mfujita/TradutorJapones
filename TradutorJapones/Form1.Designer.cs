bb4b4cecfd8e6dfff01685ebb3731e305238751f
Murilo Fujita <murilofujita@gmail.com>
2022-06-02 21:36:28 -0300

    Início em 21/05/2022

---
+++ TradutorJapones/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace TradutorJapones
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tpPort2Jap = new System.Windows.Forms.TabPage();
+            this.txtSaidaJapones = new System.Windows.Forms.TextBox();
+            this.btnPort2Jap = new System.Windows.Forms.Button();
+            this.txtEntradaPortugues = new System.Windows.Forms.TextBox();
+            this.tpJap2Port = new System.Windows.Forms.TabPage();
+            this.txtSaidaPortugues = new System.Windows.Forms.TextBox();
+            this.btnJap2Port = new System.Windows.Forms.Button();
+            this.txtEntradaJapones = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tpPort2Jap.SuspendLayout();
+            this.tpJap2Port.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tpPort2Jap);
+            this.tabControl1.Controls.Add(this.tpJap2Port);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(873, 504);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tpPort2Jap
+            // 
+            this.tpPort2Jap.Controls.Add(this.txtSaidaJapones);
+            this.tpPort2Jap.Controls.Add(this.btnPort2Jap);
+            this.tpPort2Jap.Controls.Add(this.txtEntradaPortugues);
+            this.tpPort2Jap.Location = new System.Drawing.Point(4, 27);
+            this.tpPort2Jap.Name = "tpPort2Jap";
+            this.tpPort2Jap.Padding = new System.Windows.Forms.Padding(3);
+            this.tpPort2Jap.Size = new System.Drawing.Size(865, 473);
+            this.tpPort2Jap.TabIndex = 0;
+            this.tpPort2Jap.Text = "Português para japonês";
+            this.tpPort2Jap.UseVisualStyleBackColor = true;
+            // 
+            // txtSaidaJapones
+            // 
+            this.txtSaidaJapones.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSaidaJapones.Location = new System.Drawing.Point(17, 265);
+            this.txtSaidaJapones.Multiline = true;
+            this.txtSaidaJapones.Name = "txtSaidaJapones";
+            this.txtSaidaJapones.Size = new System.Drawing.Size(825, 187);
+            this.txtSaidaJapones.TabIndex = 2;
+            // 
+            // btnPort2Jap
+            // 
+            this.btnPort2Jap.Location = new System.Drawing.Point(357, 198);
+            this.btnPort2Jap.Name = "btnPort2Jap";
+            this.btnPort2Jap.Size = new System.Drawing.Size(133, 51);
+            this.btnPort2Jap.TabIndex = 1;
+            this.btnPort2Jap.Text = "Português para japonês";
+            this.btnPort2Jap.UseVisualStyleBackColor = true;
+            this.btnPort2Jap.Click += new System.EventHandler(this.btnPort2Jap_Click);
+            // 
+            // txtEntradaPortugues
+            // 
+            this.txtEntradaPortugues.Location = new System.Drawing.Point(17, 15);
+            this.txtEntradaPortugues.Multiline = true;
+            this.txtEntradaPortugues.Name = "txtEntradaPortugues";
+            this.txtEntradaPortugues.Size = new System.Drawing.Size(825, 163);
+            this.txtEntradaPortugues.TabIndex = 0;
+            // 
+            // tpJap2Port
+            // 
+            this.tpJap2Port.Controls.Add(this.txtSaidaPortugues);
+            this.tpJap2Port.Controls.Add(this.btnJap2Port);
+            this.tpJap2Port.Controls.Add(this.txtEntradaJapones);
+            this.tpJap2Port.Location = new System.Drawing.Point(4, 27);
+            this.tpJap2Port.Name = "tpJap2Port";
+            this.tpJap2Port.Padding = new System.Windows.Forms.Padding(3);
+            this.tpJap2Port.Size = new System.Drawing.Size(865, 473);
+            this.tpJap2Port.TabIndex = 1;
+            this.tpJap2Port.Text = "Japonês para português";
+            this.tpJap2Port.UseVisualStyleBackColor = true;
+            // 
+            // txtSaidaPortugues
+            // 
+            this.txtSaidaPortugues.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSaidaPortugues.Location = new System.Drawing.Point(20, 268);
+            this.txtSaidaPortugues.Multiline = true;
+            this.txtSaidaPortugues.Name = "txtSaidaPortugues";
+            this.txtSaidaPortugues.Size = new System.Drawing.Size(825, 187);
+            this.txtSaidaPortugues.TabIndex = 5;
+            // 
+            // btnJap2Port
+            // 
+            this.btnJap2Port.Location = new System.Drawing.Point(360, 201);
+            this.btnJap2Port.Name = "btnJap2Port";
+            this.btnJap2Port.Size = new System.Drawing.Size(133, 51);
+            this.btnJap2Port.TabIndex = 4;
+            this.btnJap2Port.Text = "Japonês para português";
+            this.btnJap2Port.UseVisualStyleBackColor = true;
+            this.btnJap2Port.Click += new System.EventHandler(this.btnJap2Port_Click);
+            // 
+            // txtEntradaJapones
+            // 
+            this.txtEntradaJapones.Location = new System.Drawing.Point(20, 18);
+            this.txtEntradaJapones.Multiline = true;
+            this.txtEntradaJapones.Name = "txtEntradaJapones";
+            this.txtEntradaJapones.Size = new System.Drawing.Size(825, 163);
+            this.txtEntradaJapones.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 540);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Tradutor v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tpPort2Jap.ResumeLayout(false);
+            this.tpPort2Jap.PerformLayout();
+            this.tpJap2Port.ResumeLayout(false);
+            this.tpJap2Port.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tpPort2Jap;
+        private TabPage tpJap2Port;
+        private Button btnPort2Jap;
+        private TextBox txtEntradaPortugues;
+        private TextBox txtSaidaJapones;
+        private TextBox txtSaidaPortugues;
+        private Button btnJap2Port;
+        private TextBox txtEntradaJapones;
+    }
+}
---
+++ TradutorJapones/Form1.cs
@@ -0,0 +1,255 @@
+﻿namespace TradutorJapones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPort2Jap_Click(object sender, EventArgs e)
+        {
+            if (txtEntradaPortugues.Text != "")
+            {
+                string[] todoTexto = txtEntradaPortugues.Text.Split(" ");
+                List<string> japones = new List<string>();
+
+                for (int i = 0; i < todoTexto.Length; i++)
+                {
+                    if (todoTexto[i] == "" && todoTexto[i+1] == "") { japones.Add(" "); }
+                    else if (todoTexto[i].ToUpper() == "A") { japones.Add("あ"); }
+                    else if (todoTexto[i].ToUpper() == "E") { japones.Add("え"); }
+                    else if (todoTexto[i].ToUpper() == "I") { japones.Add("い"); }
+                    else if (todoTexto[i].ToUpper() == "O") { japones.Add("お"); }
+                    else if (todoTexto[i].ToUpper() == "U") { japones.Add("う"); }
+
+                    else if (todoTexto[i].ToUpper() == "KA") { japones.Add("か"); }
+                    else if (todoTexto[i].ToUpper() == "KE") { japones.Add("け"); }
+                    else if (todoTexto[i].ToUpper() == "KI") { japones.Add("き"); }
+                    else if (todoTexto[i].ToUpper() == "KO") { japones.Add("こ"); }
+                    else if (todoTexto[i].ToUpper() == "KU") { japones.Add("く"); }
+
+                    else if (todoTexto[i].ToUpper() == "SA") { japones.Add("さ"); }
+                    else if (todoTexto[i].ToUpper() == "SE") { japones.Add("せ"); }
+                    else if (todoTexto[i].ToUpper() == "SHI") { japones.Add("し"); }
+                    else if (todoTexto[i].ToUpper() == "SO") { japones.Add("そ"); }
+                    else if (todoTexto[i].ToUpper() == "SU") { japones.Add("す"); }
+
+                    else if (todoTexto[i].ToUpper() == "TA") { japones.Add("た"); }
+                    else if (todoTexto[i].ToUpper() == "TE") { japones.Add("て"); }
+                    else if (todoTexto[i].ToUpper() == "TI") { japones.Add("ち"); }
+                    else if (todoTexto[i].ToUpper() == "TO") { japones.Add("と"); }
+                    else if (todoTexto[i].ToUpper() == "TSU") { japones.Add("つ"); }
+
+                    else if (todoTexto[i].ToUpper() == "NA") { japones.Add("な"); }
+                    else if (todoTexto[i].ToUpper() == "NE") { japones.Add("ね"); }
+                    else if (todoTexto[i].ToUpper() == "NI") { japones.Add("に"); }
+                    else if (todoTexto[i].ToUpper() == "NO") { japones.Add("の"); }
+                    else if (todoTexto[i].ToUpper() == "NU") { japones.Add("ぬ"); }
+
+                    else if (todoTexto[i].ToUpper() == "HA") { japones.Add("は"); }
+                    else if (todoTexto[i].ToUpper() == "HE") { japones.Add("へ"); }
+                    else if (todoTexto[i].ToUpper() == "HI") { japones.Add("ひ"); }
+                    else if (todoTexto[i].ToUpper() == "HO") { japones.Add("ほ"); }
+                    else if (todoTexto[i].ToUpper() == "FU") { japones.Add("ふ"); }
+
+                    else if (todoTexto[i].ToUpper() == "MA") { japones.Add("ま"); }
+                    else if (todoTexto[i].ToUpper() == "ME") { japones.Add("め"); }
+                    else if (todoTexto[i].ToUpper() == "MI") { japones.Add("み"); }
+                    else if (todoTexto[i].ToUpper() == "MO") { japones.Add("も"); }
+                    else if (todoTexto[i].ToUpper() == "MU") { japones.Add("む"); }
+
+                    else if (todoTexto[i].ToUpper() == "RA") { japones.Add("ら"); }
+                    else if (todoTexto[i].ToUpper() == "RE") { japones.Add("れ"); }
+                    else if (todoTexto[i].ToUpper() == "RI") { japones.Add("り"); }
+                    else if (todoTexto[i].ToUpper() == "RO") { japones.Add("ろ"); }
+                    else if (todoTexto[i].ToUpper() == "RU") { japones.Add("る"); }
+
+                    else if (todoTexto[i].ToUpper() == "WA") { japones.Add("わ"); }
+                    else if (todoTexto[i].ToUpper() == "WO") { japones.Add("を"); }
+
+                    else if (todoTexto[i].ToUpper() == "N") { japones.Add("ん"); }
+
+                    else if (todoTexto[i].ToUpper() == "GA") { japones.Add("が"); }
+                    else if (todoTexto[i].ToUpper() == "GE") { japones.Add("げ"); }
+                    else if (todoTexto[i].ToUpper() == "GI") { japones.Add("ぎ"); }
+                    else if (todoTexto[i].ToUpper() == "GO") { japones.Add("ご"); }
+                    else if (todoTexto[i].ToUpper() == "GU") { japones.Add("ぐ"); }
+
+                    else if (todoTexto[i].ToUpper() == "ZA") { japones.Add("ざ"); }
+                    else if (todoTexto[i].ToUpper() == "ZE") { japones.Add("ぜ"); }
+                    else if (todoTexto[i].ToUpper() == "ZI") { japones.Add("じ"); }
+                    else if (todoTexto[i].ToUpper() == "ZO") { japones.Add("ぞ"); }
+                    else if (todoTexto[i].ToUpper() == "ZU") { japones.Add("ず"); }
+
+                    else if (todoTexto[i].ToUpper() == "DA") { japones.Add("だ"); }
+                    else if (todoTexto[i].ToUpper() == "DE") { japones.Add("で"); }
+                    else if (todoTexto[i].ToUpper() == "DI") { japones.Add("ぢ"); }
+                    else if (todoTexto[i].ToUpper() == "DO") { japones.Add("ど"); }
+                    else if (todoTexto[i].ToUpper() == "DU") { japones.Add("づ"); }
+
+                    else if (todoTexto[i].ToUpper() == "BA") { japones.Add("ば"); }
+                    else if (todoTexto[i].ToUpper() == "BE") { japones.Add("べ"); }
+                    else if (todoTexto[i].ToUpper() == "BI") { japones.Add("び"); }
+                    else if (todoTexto[i].ToUpper() == "BO") { japones.Add("ぼ"); }
+                    else if (todoTexto[i].ToUpper() == "BU") { japones.Add("ぶ"); }
+
+                    else if (todoTexto[i].ToUpper() == "PA") { japones.Add("ぱ"); }
+                    else if (todoTexto[i].ToUpper() == "PE") { japones.Add("ぺ"); }
+                    else if (todoTexto[i].ToUpper() == "PI") { japones.Add("ぴ"); }
+                    else if (todoTexto[i].ToUpper() == "PO") { japones.Add("ぽ"); }
+                    else if (todoTexto[i].ToUpper() == "PU") { japones.Add("ぷ"); }
+
+                    else if (todoTexto[i].ToUpper() == "JA") { japones.Add("じゃ"); }
+                    else if (todoTexto[i].ToUpper() == "JO") { japones.Add("じょ"); }
+                    else if (todoTexto[i].ToUpper() == "JU") { japones.Add("じゅ"); }
+                }
+
+                foreach (var item in japones)
+                {
+                    txtSaidaJapones.Text += item.ToString();
+                }
+            }
+        }
+
+        private void btnJap2Port_Click(object sender, EventArgs e)
+        {
+            if (txtEntradaJapones.Text != "")
+            {
+                char[] todoTexto = txtEntradaJapones.Text.ToCharArray();
+                List<string> portugues = new List<string>();
+
+                for (int i = 0; i < todoTexto.Length; i++)
+                {
+                    if (todoTexto[i] == ' ') { portugues.Add(" "); }
+
+                    else if (todoTexto[i] == 'ゃ')
+                    {
+                        if (todoTexto[i-1] == 'じ')
+                        {
+                            portugues.Add("Ja");
+                            i++;
+                        }
+                    }
+
+                    else if (todoTexto[i] == 'ょ')
+                    {
+                        if (todoTexto[i - 1] == 'じ')
+                        {
+                            
+                            i++;
+                            portugues.Add("Jo");
+                        }
+                    }
+
+                    else if (todoTexto[i] == 'ゅ')
+                    {
+                        if (todoTexto[i - 1] == 'じ')
+                        {
+                            portugues.Add("Ju");
+                            i++;
+                        }
+                    }
+
+                    else if (todoTexto[i] == 'あ') { portugues.Add("A"); }
+                    else if (todoTexto[i] == 'え') { portugues.Add("E"); }
+                    else if (todoTexto[i] == 'い') { portugues.Add("I"); }
+                    else if (todoTexto[i] == 'お') { portugues.Add("O"); }
+                    else if (todoTexto[i] == 'う') { portugues.Add("U"); }
+
+                    else if (todoTexto[i] == 'か') { portugues.Add("KA"); }
+                    else if (todoTexto[i] == 'け') { portugues.Add("KE"); }
+                    else if (todoTexto[i] == 'き') { portugues.Add("KI"); }
+                    else if (todoTexto[i] == 'こ') { portugues.Add("KO"); }
+                    else if (todoTexto[i] == 'く') { portugues.Add("KU"); }
+
+                    else if (todoTexto[i] == 'さ') { portugues.Add("SA"); }
+                    else if (todoTexto[i] == 'せ') { portugues.Add("SE"); }
+                    else if (todoTexto[i] == 'し') { portugues.Add("SHI"); }
+                    else if (todoTexto[i] == 'そ') { portugues.Add("SO"); }
+                    else if (todoTexto[i] == 'す') { portugues.Add("SU"); }
+
+                    else if (todoTexto[i] == 'た') { portugues.Add("TA"); }
+                    else if (todoTexto[i] == 'て') { portugues.Add("TE"); }
+                    else if (todoTexto[i] == 'ち') { portugues.Add("TI"); }
+                    else if (todoTexto[i] == 'と') { portugues.Add("TO"); }
+                    else if (todoTexto[i] == 'つ') { portugues.Add("TSU"); }
+
+                    else if (todoTexto[i] == 'な') { portugues.Add("NA"); }
+                    else if (todoTexto[i] == 'ね') { portugues.Add("NE"); }
+                    else if (todoTexto[i] == 'に') { portugues.Add("NI"); }
+                    else if (todoTexto[i] == 'の') { portugues.Add("NO"); }
+                    else if (todoTexto[i] == 'ぬ') { portugues.Add("NU"); }
+
+                    else if (todoTexto[i] == 'は') { portugues.Add("HA"); }
+                    else if (todoTexto[i] == 'へ') { portugues.Add("HE"); }
+                    else if (todoTexto[i] == 'ひ') { portugues.Add("HI"); }
+                    else if (todoTexto[i] == 'ほ') { portugues.Add("HO"); }
+                    else if (todoTexto[i] == 'ふ') { portugues.Add("FU"); }
+
+                    else if (todoTexto[i] == 'ま') { portugues.Add("MA"); }
+                    else if (todoTexto[i] == 'め') { portugues.Add("ME"); }
+                    else if (todoTexto[i] == 'み') { portugues.Add("MI"); }
+                    else if (todoTexto[i] == 'も') { portugues.Add("MO"); }
+                    else if (todoTexto[i] == 'む') { portugues.Add("MU"); }
+
+                    else if (todoTexto[i] == 'ら') { portugues.Add("RA"); }
+                    else if (todoTexto[i] == 'れ') { portugues.Add("RE"); }
+                    else if (todoTexto[i] == 'り') { portugues.Add("RI"); }
+                    else if (todoTexto[i] == 'ろ') { portugues.Add("RO"); }
+                    else if (todoTexto[i] == 'る') { portugues.Add("RU"); }
+
+                    else if (todoTexto[i] == 'わ') { portugues.Add("WA"); }
+                    else if (todoTexto[i] == 'を') { portugues.Add("WO"); }
+
+                    else if (todoTexto[i] == 'ん') { portugues.Add("N"); }
+
+                    else if (todoTexto[i] == 'が') { portugues.Add("GA"); }
+                    else if (todoTexto[i] == 'げ') { portugues.Add("GE"); }
+                    else if (todoTexto[i] == 'ぎ') { portugues.Add("GI"); }
+                    else if (todoTexto[i] == 'ご') { portugues.Add("GO"); }
+                    else if (todoTexto[i] == 'ぐ') { portugues.Add("GU"); }
+
+                    else if (todoTexto[i] == 'ざ') { portugues.Add("ZA"); }
+                    else if (todoTexto[i] == 'ぜ') { portugues.Add("ZE"); }
+                    else if (todoTexto[i] == 'じ') { portugues.Add("ZI"); }
+                    else if (todoTexto[i] == 'ぞ') { portugues.Add("ZO"); }
+                    else if (todoTexto[i] == 'ず') { portugues.Add("ZU"); }
+
+                    else if (todoTexto[i] == 'だ') { portugues.Add("DA"); }
+                    else if (todoTexto[i] == 'で') { portugues.Add("DE"); }
+                    else if (todoTexto[i] == 'ぢ') { portugues.Add("DI"); }
+                    else if (todoTexto[i] == 'ど') { portugues.Add("DO"); }
+                    else if (todoTexto[i] == 'づ') { portugues.Add("DU"); }
+
+                    else if (todoTexto[i] == 'ば') { portugues.Add("BA"); }
+                    else if (todoTexto[i] == 'べ') { portugues.Add("BE"); }
+                    else if (todoTexto[i] == 'び') { portugues.Add("BI"); }
+                    else if (todoTexto[i] == 'ぼ') { portugues.Add("BO"); }
+                    else if (todoTexto[i] == 'ぶ') { portugues.Add("BU"); }
+
+                    else if (todoTexto[i] == 'ぱ') { portugues.Add("PA"); }
+                    else if (todoTexto[i] == 'ぺ') { portugues.Add("PE"); }
+                    else if (todoTexto[i] == 'ぴ') { portugues.Add("PI"); }
+                    else if (todoTexto[i] == 'ぽ') { portugues.Add("PO"); }
+                    else if (todoTexto[i] == 'ぷ') { portugues.Add("PU"); }
+
+                    
+                    //else if (todoTexto[i] == 'じゃ') { portugues.Add("JA"); }
+                    //else if (todoTexto[i] == 'じょ') { portugues.Add("JO"); }
+                    //else if (todoTexto[i] == 'じゅ') { portugues.Add("JU"); }
+                    }
+
+                foreach (var item in portugues)
+                {
+                    txtSaidaPortugues.Text += item;
+                }
+            }
+        }
+    }
+}
